f4dfc5f89952e03621850d62146f266813b98752
yahiya_mansuri <yahiyamansuri14@gmail.com>
2021-05-14 10:44:24 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const DB_URL = "YOUR URL"
+const DB_URL = "mongodb+srv://yahiyamansuri:Mongo1998@yahiya-cluster.hjixa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 //connecting to remote mongo db database 
 mongoose.connect(DB_URL, {useNewUrlParse:true,useUnifiedTopology: true}).then(con=>{
     console.log("database connected")
